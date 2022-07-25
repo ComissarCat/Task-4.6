399170416e8ff58908c54d4132160c504ed0f3fe
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2022-07-25 17:16:43 +0300

    Добавьте файлы проекта.

---
+++ Task 4.6/Task 4.6.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    task_1();
+
+    return 0;
+}
+
+void task_1()
+{
+    char symbol = ' ', direction = ' ';
+    int length = 0, speed = 0;    
+    cout << "Введите символ: ";
+    cin >> symbol;
+    do
+    {
+        cout << "Введите длину линии: ";
+        cin >> length;
+    } while (length < 1);
+    do
+    {
+        cout << "Введите направление линии (v/h): ";
+        cin >> direction;        
+    } while (direction != 'v' and direction != 'h');
+    do
+    {
+        cout << "Введите скорость отрисовки 1 - 3: ";
+        cin >> speed;
+    } while (speed < 1 or speed > 3);
+    system("cls");
+    for (int i = 0; i < length; i++)
+    {
+        cout << symbol;
+        if (direction == 'v') cout << endl;
+        else cout << "\t";
+        Sleep(speed * 100);
+    }
+}
