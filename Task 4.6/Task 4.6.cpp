ae9563e556e4b581edc4e991e32b65783555455f
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2022-07-26 09:44:15 +0300

    Реализована вторая задача (игра в кости)

--- Task 4.6/Task 4.6.cpp
+++ Task 4.6/Task 4.6.cpp
@@ -6,7 +6,8 @@ void task_1();
 void task_2();
 void task_3();
 void task_4();
-void draw_cube(int value);
+void draw_dice(int value);
+void dice_roll(bool& player_move, int& player_score, int& computer_score);
 
 int main()
 {
@@ -50,12 +51,26 @@ void task_1()
 
 void task_2()
 {
-    draw_cube(3);
+    int player_score = 0, computer_score = 0;
+    bool player_move = false;
+    for (int i = 0; i < 6; i++)
+    {
+        system("cls");
+        cout << "Игрок\t|\tКомпьютер\n";
+        cout << player_score << "\t|\t" << computer_score << "\n\n";
+        system("pause");
+        dice_roll(player_move, player_score, computer_score);
+    }
+    system("cls");
+    cout << "Со счётом " << player_score << " : " << computer_score;
+    if (player_score > computer_score) cout << " победил игрок!";
+    else if (player_score < computer_score) cout << " победил компьютер!";
+    else cout << " игра закончилась ничьей!";
+    cout << "\n\n";
 }
 
-void draw_cube(int value)
-{
-    system("cls");
+void draw_dice(int value)
+{    
     switch (value)
     {
     case(1):
@@ -113,4 +128,28 @@ void draw_cube(int value)
     }
     break;
     }
+    system("pause");
+}
+
+void dice_roll(bool& player_move, int& player_score, int& computer_score)
+{
+    int value = 0;
+    system("cls");
+    if (player_move)
+    {
+        cout << "Ход игрока\n\n";
+        system("pause");
+        cout << "\n\n";
+    }
+    else
+    {
+        cout << "Ход компьютера\n\n";
+        Sleep(1000);
+    }
+    srand(time(NULL));
+    value = rand() % 6 + 1;
+    if (player_move) player_score += value;
+    else computer_score += value;
+    player_move = !player_move;
+    draw_dice(value);
 }
