e4318fca8d74bdb9ac61fd23c89477d318d66f9c
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2022-07-26 15:45:16 +0300

    Решены 3 и 4 задачи (игра в угадай число)

--- Task 4.6/Task 4.6.cpp
+++ Task 4.6/Task 4.6.cpp
@@ -8,12 +8,13 @@ void task_3();
 void task_4();
 void draw_dice(int value);
 void dice_roll(bool& player_move, int& player_score, int& computer_score);
+void round(int level, bool upgraded, int& score, int& computer_score);
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    task_2();
+    task_4();
 
     return 0;
 }
@@ -152,4 +153,115 @@ void dice_roll(bool& player_move, int& player_score, int& computer_score)
     else computer_score += value;
     player_move = !player_move;
     draw_dice(value);
+}
+
+void task_3()
+{
+    int level = 0, score = 0, computer_score = 0;
+    bool upgraded = false;
+    do
+    {
+        system("cls");
+        cout << "Введите уровень сложности (1/2): ";
+        cin >> level;
+    } while (level < 1 or level > 2);
+    round(level, upgraded, score, computer_score);
+}
+
+void task_4()
+{
+    int level = 1, score = 0, computer_score = 0;
+    bool upgraded = true;
+    char continuation = ' ';
+    for (int i = 0; i < 3; i++)
+    {
+        round(level, upgraded, score, computer_score);
+        do
+        {
+            cout << "\n\nХотите продолжить? y/n ";
+            cin >> continuation;
+        } while (continuation != 'y' and continuation != 'n');
+        if (continuation == 'n') break;
+    }    
+    if (continuation == 'y')
+    {
+        level = 2;
+        for (int i = 0; i < 2; i++)
+        {
+            round(level, upgraded, score, computer_score);
+            if (i != 1)
+            {
+                do
+                {
+                    cout << "\n\nХотите продолжить? y/n ";
+                    cin >> continuation;
+                } while (continuation != 'y' and continuation != 'n');
+                if (continuation == 'n') break;
+            }
+        }
+    }
+    system("cls");
+    cout << "Ваш счет : " << score << ", счет компьютера: " << computer_score;
+    cout << "\n\n";
+    system("pause");
+}
+
+void round(int level, bool upgraded, int& score, int& computer_score)
+{
+    int start = 0, end = 0, hp = 0, target_number = 0, player_number = 0, used_hints = 0;
+    char hint = ' ';
+    srand(time(NULL));
+    if (level == 1)
+    {
+        hp = 5;
+        target_number = rand() % 10 + 1;
+        start = 1, end = 10;
+    }
+    else
+    {
+        hp = 22;
+        target_number = rand() % 91 + 10;
+        start = 10, end = 100;
+    }
+    for (int i = 1;;)
+    {
+        system("cls");
+        cout << "Осталось жизней: " << hp << ", использовано подсказок " << used_hints << "\n\n";
+        cout << "Какое число загадал компьютер в промежутке от " << start << " до " << end << "?\n";
+        cin >> player_number;
+        if (player_number == target_number)
+        {
+            system("cls");
+            if (level == 1) score += hp * 5;
+            else score += hp * 10;
+            cout << "Число угадано с " << i << " попытки, использовано подсказок " << used_hints << ", ваш счет: " << score;
+            if (upgraded) cout << ", счет компьютера: " << computer_score;
+            cout << "\n\n";
+            system("pause");
+            break;
+        }
+        i++, hp--;
+        do
+        {
+            cout << "Хотите взять подсказку? y/n ";
+            cin >> hint;
+        } while (hint != 'y' and hint != 'n');
+        if (hint == 'y')
+        {
+            hp--, used_hints++;
+            if (player_number > target_number) cout << "\nЗагаданное число меньше\n";
+            else cout << "\nЗагаданное число больше\n";
+            system("pause");
+        }
+        if (hp < 1)
+        {
+            system("cls");
+            if (level == 1) computer_score += 5 * 5;
+            else computer_score += 22 * 10;
+            cout << "Смэрт от неугадания\n\n";
+            if (upgraded) cout << "Ваш счет: " << score << ", счет компьютера: " << computer_score << "\n\n";
+            system("pause");
+            break;
+        }
+    }
 }
